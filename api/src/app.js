8f2dd7ce8d3a9305ba649150358d19dcfd17c923
SLMM <sergio.marques@puc-campinas.edu.br>
2026-05-10 23:12:00 -0300

    ss

--- api/src/app.js
+++ api/src/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 // checa se api no ar
 app.get('/health', (req, res) => {
-  res.json({ status: 'ok', timestamp: new Date().toISOString() , by:'SLMM6'});
+  res.json({ status: 'ok', timestamp: new Date().toISOString() , by:'SLMM7'});
 });
 
 app.get('/api/tabelas', (req, res) => {
