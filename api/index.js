2ceac13ab5d0fb84e43785245331031b89e24afc
sergiolmm-pucc <104084417+sergiolmm-pucc@users.noreply.github.com>
2026-05-14 00:03:52 -0300

    Update console log message for API startup

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const app = require('./src/app');
 const PORT = process.env.PORT || 3001;
 
 app.listen( PORT, () => {
-  console.log(` API rodando em   ${PORT}`); 
+  console.log(` API rodando em ${PORT} ajuste 1`); 
   console.log(` para verificar saude da API digite  : `);
   console.log(` http://localhost:${PORT}/health`);	
 
